74c31b648aeaae1d9ce8aba8dae5dd1b9e5c531e
Aleksandr Karev <w88190@gmail.com>
2022-06-24 23:56:41 +0700

    добавить 3 основных попапа (императвный подход)

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,17 +1,32 @@
 
+function handleEditAvatarClick() {
+    const popupEditAvatar = document.querySelector('.popup_type_update');
+    popupEditAvatar.classList.add('popup_opened');
+}
+
+function handleEditProfileClick() {
+    const popupEditProfile = document.querySelector('.popup_type_profile');
+    popupEditProfile.classList.add('popup_opened');
+}
+
+function handleAddPlaceClick() {
+    const popupAddPlace = document.querySelector('.popup_type_place');
+    popupAddPlace.classList.add('popup_opened');
+}
+
 function Main() {
     return (
         <main className="main">
             <section className="profile">
                 <div className="profile__avatar-container">
                     <div className="profile__avatar"/>
-                    <button className="profile__avatar-button" type="button" aria-label="редактировать аватар"/>
+                    <button className="profile__avatar-button" onClick={handleEditAvatarClick} type="button" aria-label="редактировать аватар"/>
                 </div>
                 <div className="profile__info">
                     <h1 className="profile__title">Felix Baumgartner</h1>
-                    <button className="profile__edit-button" type="button" aria-label="редактировать профиль"/>
+                    <button className="profile__edit-button" onClick={handleEditProfileClick} type="button" aria-label="редактировать профиль"/>
                 </div>
-                <button className="profile__add-button" type="button" aria-label="добавить"/>
+                <button className="profile__add-button" onClick={handleAddPlaceClick} type="button" aria-label="добавить"/>
                 <p className="profile__subtitle">Red Bull Stratos</p>
             </section>
             <section className="elements">
